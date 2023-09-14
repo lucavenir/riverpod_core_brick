c995a13002ae45276cb0510d84eace95e5ed45de
venir.dev <luca@venir.dev>
2023-09-15 00:01:24 +0200

    Simplified the post gen hook

--- brick/hooks/post_gen.dart
+++ brick/hooks/post_gen.dart
@@ -1,5 +1,10 @@
+import 'dart:io' as io;
+
 import 'package:mason/mason.dart';
 
-void run(HookContext context) {
-  // TODO: add post-generation logic.
+Future<void> run(HookContext context) async {
+  // Run `flutter packages get` after generation.
+  final progress = context.logger.progress('Installing packages');
+  await io.Process.run('flutter', ['packages', 'get']);
+  progress.complete();
 }
